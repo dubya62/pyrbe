76a937974bbceb88040f4901400566b4d3119a00
dubya <dubya297@yahoo.com>
2024-12-15 17:29:59 -0600

    Added the final ideas for the syntax and regex rules.

--- pyrbe.py
+++ pyrbe.py
@@ -1,8 +1,30 @@
 
 
+class Rule:
+    def __init__(self):
+        pass
+
+
+class RBE:
+    def __init__(self):
+        self.rules:list[Rule] = []
+
+    # should allow reading rules from a human readable file
+    def read_from_file(self, filename:str) -> list[Rule]:
+        try:
+            with open(filename, "r") as f:
+                the_file = f.read()
+        except:
+            print(f"Could not open file ({the_file})")
+            exit(0)
+
+        return result
+
+
 
 
 
 
 if __name__ == "__main__":
-    pass
+    rbe = RBE()
+
