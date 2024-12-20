1753fca86121dfd15ce60dc9effa5f1803526fa6
dubya <dubya297@yahoo.com>
2024-12-20 17:19:22 -0600

    Still working on the ? and ! matching.

--- pyrbe.py
+++ pyrbe.py
@@ -16,6 +16,18 @@ class MatchResult:
         return self.__str__()
 
 
+class MatchRule:
+    def __init__(self, clause:list[str], min_reps:list[int], max_reps:list[int], matchers:list[object], match_openers:list[bool], match_closers:list[bool], store:list[int], get:list[list[int]]):
+        self.clause = clause
+        self.min_repetitions = min_reps
+        self.max_repetitions = max_reps
+        self.matchers = matchers
+        self.match_openers = match_openers
+        self.match_closers = match_closers
+        self.store = store
+        self.get = get
+
+
 class Rule:
     def __init__(self, clauses:list[str], condition:str):
         self.clauses = clauses
@@ -50,21 +62,147 @@ class Rule:
         # convert a clause into a much faster list of instructions
         # should be able to call this as a function
         # input a list of tokens -> where the match was found and the length of the match
-        instructions = []
         clause = clause.strip('"').split(" ")
         store = []
         get = []
+        matchers = [None] * len(clause) # list of functions that return True or false depending on if a token matches it
         min_repetitions = [1] * len(clause)
         max_repetitions = [1] * len(clause)
-        print(clause)
-        print(min_repetitions)
-        print(max_repetitions)
+        match_openers = [False] * len(clause)
+        match_closers = [False] * len(clause)
 
+        def ANY(token:str):
+            return True
+
+        def create_contains_fun(token_list:list[str]):
+            return lambda tok: tok in token_list
+
+        current_token = 0
         for x in clause:
-            if "$" in x:
-                pass
-        
-        pass
+            backslashes = 0
+            match_token = ""
+            match_tokens = []
+            the_fun = None
+            for y in range(len(x)):
+                if x[y] == "\\":
+                    backslashes += 1
+                elif x[y] == ".":
+                    if backslashes % 2 == 0:
+                        the_fun = ANY
+                        backslashes = 0
+                        continue
+                elif x[y] == "*":
+                    if backslashes % 2 == 0:
+                        min_repetitions[current_token] = 0
+                        max_repetitions[current_token] = None
+                        backslashes = 0
+                        continue
+                elif x[y] == "+":
+                    if backslashes % 2 == 0:
+                        min_repetitions[current_token] = 1
+                        max_repetitions[current_token] = None
+                        backslashes = 0
+                        continue
+                elif x[y] == "|":
+                    if backslashes % 2 == 0:
+                        match_tokens.append(match_token)
+                        match_token = ""
+                        backslashes = 0
+                        continue
+                elif x[y] == "$":
+                    if backslashes % 2 == 0:
+                        store_index = x[y+1:]
+                        try:
+                            store_index = int(store_index)
+                        except:
+                            print(f"Invalid store index '{store_index}'")
+                            exit(1)
+                        while len(store) <= store_index:
+                            store.append(None)
+                            get.append([])
+                        store[store_index] = current_token
+                        backslashes = 0
+                        break
+                elif x[y] == "%":
+                    if backslashes % 2 == 0:
+                        get_index = x[y+1:]
+                        try:
+                            get_index = int(get_index)
+                        except:
+                            print(f"Invalid get index '{get_index}'")
+                            exit(1)
+                        while len(get) <= get_index:
+                            store.append(None)
+                            get.append([])
+                        get[get_index].append(current_token)
+                        backslashes = 0
+                        break
+                elif x[y] == "{":
+                    if backslashes % 2 == 0:
+                        if y + 2 >= len(x):
+                            print("ERROR: expected size after {")
+                            exit(1)
+                        first = ""
+                        second = ""
+                        switched = 0
+                        for z in range(y+1, len(x)):
+                            if x[z] == ",":
+                                switched = 1
+                                continue
+                            elif x[z] == "}":
+                                break
+                            if switched:
+                                second += x[z]
+                            else:
+                                first += x[z]
+                        if first == "":
+                            first = 1
+                        if second == "":
+                            second = first
+                        backslashes = 0
+                        break
+                elif x[y] == "?":
+                    if backslashes % 2 == 0:
+                        match_openers[current_token] = True
+                        backslashes = 0
+                        break
+                elif x[y] == "!":
+                    if backslashes % 2 == 0:
+                        match_closers[current_token] = True
+                        backslashes = 0
+                        break
+
+
+                if x[y] != "\\":
+                    backslashes = 0
+
+                match_token += x[y]
+
+
+            if len(match_token) > 0:
+                match_tokens.append(match_token)
+
+            if the_fun == None:
+                matchers[current_token] = create_contains_fun(match_tokens)
+            else:
+                matchers[current_token] = the_fun
+
+            print(match_tokens)
+
+
+            current_token += 1
+
+
+        print("Clause", clause)
+        print("min_reps", min_repetitions)
+        print("max_reps", max_repetitions)
+        print("matchers", matchers)
+        print("match_openers", match_openers)
+        print("match_closers", match_closers)
+        print("store", store)
+        print("get", get)
+        result = MatchRule(clause, min_repetitions, max_repetitions, matchers, match_openers, match_closers, store, get)
+        return result
 
 
     def match(self, tokens:list[str]):
